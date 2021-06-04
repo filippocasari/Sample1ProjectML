8a6b12ccc9eb9fc806e6b04018cbe55b11d14f13
Filippo <fili9941@gmail.com>
2021-06-04 17:14:54 +0200

    updates

--- Car_GaussianNB.py
+++ Car_GaussianNB.py
@@ -1,42 +1,43 @@
+from matplotlib import pyplot as plt
 from scipy import sparse
-from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix, precision_recall_curve, roc_curve
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-from sklearn.neural_network import MLPClassifier
 import numpy as np
-from sklearn import preprocessing
+
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, LabelBinarizer, MinMaxScaler, StandardScaler
-from sklearn.utils import class_weight
+
 
 input_file = "CAR/car.data"
 df = pd.read_csv(input_file, header=0)
 
 dataset = df.values
-
+print(df.info())
+print(df.describe())
 X = dataset[:, 0:6]
 Y = dataset[:, 6]
 print("X:\n", X)
 print("Y:\n", Y)
-switch_buying = {'vhigh': 1,
-                 'high': 2,
-                 'med': 3,
-                 'low': 4
+switch_buying = {'vhigh': 0,
+                 'high': 1,
+                 'med': 2,
+                 'low': 3
                  }
-switch_maint = {'vhigh': 1,
-                'high': 2,
-                'med': 3,
-                'low': 4
+switch_maint = {'vhigh': 0,
+                'high': 1,
+                'med': 2,
+                'low': 3
                 }
-switch_doors = {'2': 1,
-                '3': 2,
-                '4': 3,
-                '5more': 5
+switch_doors = {'2': 0,
+                '3': 1,
+                '4': 2,
+                '5more': 3
                 }
 
-switch_persons = {'2': 2,
-                  '4': 3,
-                  'more': 4
+switch_persons = {'2': 0,
+                  '4': 1,
+                  'more': 2
                   }
 switch_lug_boot = {
     'small': 1,
@@ -62,14 +63,15 @@ switch_Y = {
     'good': 3,
     'vgood': 4
 }
-X_final = np.empty(shape=np.shape(Y), dtype=int)
+X_final = np.empty(shape=np.shape(X), dtype='int64')
 for i in range(len(X)):
 
     for j in range(len(X[i])):
         switch_temp = switch[j]
-        X[i][j] = switch_temp[X[i][j]]
+        X_final[i][j] = switch_temp[X[i][j]]
 
-print(X)
+print("Original X:\n",X)
+print("X:\n",X_final)
 '''
 for i in range(len(Y)):
     Y[i]=switch_Y[Y[i]]
@@ -77,9 +79,9 @@ for i in range(len(Y)):
 '''
 print("X : \n", X)
 # --------------------------Pre-processing--------------------------------
-X_normalized = MinMaxScaler().fit_transform(X)
+X_normalized = MinMaxScaler().fit_transform(X_final)
 print("X normalized: \n", X_normalized)
-X_standardized = StandardScaler().fit_transform(X)
+X_standardized = StandardScaler().fit_transform(X_final)
 print("X standardized: \n", X_standardized)
 # ------------------------------------------------------------------------
 le = LabelEncoder()
@@ -90,7 +92,7 @@ print(list(le.classes_))
 # No preprocessing
 X_train, X_test, Y_train, \
 Y_test = \
-    train_test_split(X, Y, test_size=0.3)
+    train_test_split(X_final, Y, test_size=0.3)
 
 # ------------------------------------------------------------------------
 # Yes preprocessing
@@ -132,10 +134,10 @@ gnb_weighted_no_preprocess = GaussianNB(priors=[weight_class_1, weight_class_2, 
 # NO WEIGHT, NO PREPROCESSING
 gnb_not_balanced = GaussianNB().fit(X_train, Y_train)
 # BALANCING WITH WEIGHT,PREPROCESSING, NORMAL
-gdb_weighted_preprocc_norm = GaussianNB(priors=[weight_class_1, weight_class_2, weight_class_3, weight_class_4]).fit(
+gdb_weighted_preprocc_norm = GaussianNB().fit(
     X_train_NORM, Y_train_NORM)
-# BALANCING WITH WEIGHT,PREPROCESSING, STAND
-gdb_weighted_preprocc_stand = GaussianNB(priors=[weight_class_1, weight_class_2, weight_class_3, weight_class_4]).fit(
+# BALANCING WITH WEIGHT,PREPROCESSING, STAND----priors=[weight_class_1, weight_class_2, weight_class_3, weight_class_4]
+gdb_weighted_preprocc_stand = GaussianNB().fit(
     X_train_STAND, Y_train_STAND)
 
 # --------------------PREDICTIONS---------------------------------
@@ -153,13 +155,13 @@ target_names = ['unacc', 'acc', 'good', 'vgood']
 # ---------------------------------PRINTING-----------------------------------------
 
 print("-----------------------WITH-BALANCING-Normalization-Weighted----------------------------")
-print(classification_report(Y_test, gnb_predictions_norm, target_names=target_names))
+print(classification_report(Y_test_NORM, gnb_predictions_norm, labels=target_names))
 print("-----------------------WITH-BALANCING-Standardization-Weighted----------------------------")
-print(classification_report(Y_test, gnb_predictions_stand, target_names=target_names))
+print(classification_report(Y_test_STAND, gnb_predictions_stand, labels=target_names))
 print("-----------------------WITH NO BALANCING, NO PREPROCESSING----------------------------")
-print(classification_report(Y_test, gnb_predictions_no_balancing, target_names=target_names))
+print(classification_report(Y_test, gnb_predictions_no_balancing, labels=target_names))
 print("-----------------------WITH BALANCING WEIGHTS, NO PREPROCESSING----------------------------")
-print(classification_report(Y_test, gnb_predictions, target_names=target_names))
+print(classification_report(Y_test, gnb_predictions, labels=target_names))
 Num_of_instances = {'unacc': 0, 'acc': 0, 'good': 0, 'vgood': 0}
 class1_num_y_test = 0
 class2_num_y_test = 0
@@ -187,3 +189,33 @@ print("Instances of class 'good' ", class3_num_y_train)
 print("Instances of class 'vgood' ", class4_num_y_train)
 # creating a confusion matrix
 cm = confusion_matrix(Y_test, gnb_predictions)
+precision = dict()
+recall = dict()
+n_classes=4
+gnb_predictions=LabelBinarizer().fit_transform(gnb_predictions)
+print(gnb_predictions)
+Y_test=LabelBinarizer().fit_transform(Y_test)
+for i in range(n_classes):
+    precision[i], recall[i], _ = precision_recall_curve(Y_test[:, i],
+                                                        gnb_predictions[:, i])
+    plt.plot(recall[i], precision[i], lw=2, label='class {}'.format(i))
+
+plt.xlabel("recall")
+plt.ylabel("precision")
+plt.legend(loc="best")
+plt.title("precision vs. recall curve")
+plt.show()
+# roc curve
+fpr = dict()
+tpr = dict()
+
+for i in range(n_classes):
+    fpr[i], tpr[i], _ = roc_curve(Y_test[:, i],
+                                  gnb_predictions[:, i])
+    plt.plot(fpr[i], tpr[i], lw=2, label='class {}'.format(i))
+
+plt.xlabel("false positive rate")
+plt.ylabel("true positive rate")
+plt.legend(loc="best")
+plt.title("ROC curve")
+plt.show()
