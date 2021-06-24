d337f75676c031a39580b9e23a642fcf3b3dca8e
filippocasari <fili9941@gmail.com>
2021-06-24 12:42:37 +0200

    bella

--- Car_class.py
+++ Car_class.py
@@ -9,20 +9,11 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder, LabelBinarizer
 from sklearn.utils import class_weight
 
-input_file = "CAR/car.data"
-df = pd.read_csv(input_file, header=0)
-
-dataset = df.values
-
-X = dataset[:, 0:6]
-Y = dataset[:, 6]
-print("X:\n", X)
-print("Y:\n", Y)
 switch_buying = {'vhigh': 1,
-                 'high': 2,
-                 'med': 3,
-                 'low': 4
-                 }
+                     'high': 2,
+                     'med': 3,
+                     'low': 4
+                     }
 switch_maint = {'vhigh': 1,
                 'high': 2,
                 'med': 3,
@@ -48,62 +39,91 @@ switch_safety = {
     'med': 2,
     'high': 3
 }
-switch={
-    0:switch_buying,
-    1:switch_maint,
-    2:switch_doors,
-    3:switch_persons,
-    4:switch_lug_boot,
-    5:switch_safety
+switch = {
+    0: switch_buying,
+    1: switch_maint,
+    2: switch_doors,
+    3: switch_persons,
+    4: switch_lug_boot,
+    5: switch_safety
 }
-switch_Y={
-    'unacc':1,
-    'acc':2,
-    'good':3,
-    'vgood':4
+
+def trasform_matrix(x):
+
+    for i in range(len(x)):
+
+        for j in range(len(x[i])):
+            switch_temp = switch[j]
+            x[i][j] = switch_temp[x[i][j]]
+
+    print(x)
+    return x
+
+
+
+input_file = "CAR/car.data"
+df = pd.read_csv(input_file, header=0)
+
+dataset = df.values
+
+X = dataset[:, 0:6]
+Y = dataset[:, 6]
+print("X:\n", X)
+print("Y:\n", Y)
+
+
+switch_Y = {
+    'unacc': 1,
+    'acc': 2,
+    'good': 3,
+    'vgood': 4
 }
 X_final = np.empty(shape=np.shape(Y), dtype=int)
-for i in range(len(X)):
 
-    for j in range(len(X[i])):
 
-        switch_temp= switch[j]
-        X[i][j]=switch_temp[X[i][j]]
 
-print(X)
+
+
+X = trasform_matrix(x=X)
 '''
 for i in range(len(Y)):
     Y[i]=switch_Y[Y[i]]
 
 '''
-le =LabelEncoder()
+le = LabelEncoder()
 le.fit_transform(Y)
 
 X_train, X_test, Y_train, \
-    Y_test = \
-        train_test_split(X, Y, test_size=0.3)
+Y_test = \
+    train_test_split(X, Y, test_size=0.3)
 print(list(le.classes_))
 class_weights = class_weight.compute_class_weight('balanced',
-                                                 np.unique(Y_train),
-                                                 Y_train)
+                                                  np.unique(Y_train),
+                                                  Y_train)
 y_dense = LabelBinarizer().fit_transform(Y)
 
 y_sparse = sparse.csr_matrix(y_dense)
 print(y_sparse)
 
-
-
-
-#min_max_scaler = preprocessing.MinMaxScaler()
-#X_train_minmax = min_max_scaler.fit_transform(X_train)
-#X_test_minmax =min_max_scaler.fit_transform(X_test)
+# min_max_scaler = preprocessing.MinMaxScaler()
+# X_train_minmax = min_max_scaler.fit_transform(X_train)
+# X_test_minmax =min_max_scaler.fit_transform(X_test)
 gnb = GaussianNB().fit(X_train, Y_train)
 
 gnb_predictions = gnb.predict(X_test)
 
 # accuracy on X_test
-accuracy = gnb.score(X_test, Y_test)
-print (accuracy)
+accuracy = accuracy_score(Y_test, gnb_predictions, normalize=True)
+print("Accuracy :" ,accuracy)
 
 # creating a confusion matrix
 cm = confusion_matrix(Y_test, gnb_predictions)
+x_sample1 = np.array([['low', 'low', '5more', 'more', 'big', 'high']])
+x_sample2=np.array([['vhigh','vhigh','2','2','med','low']])
+x_sample1 = trasform_matrix(x=x_sample2)
+y_sample1_predict = gnb.predict(x_sample2)
+print(y_sample1_predict)
+accuracy_sample_1 = accuracy_score(['unacc'], y_sample1_predict)
+print(accuracy_sample_1)
+print(cm)
+exit()
--- main.py
+++ main.py
@@ -1,105 +1,74 @@
+from sklearn.feature_selection import SelectKBest, chi2, SelectFromModel
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 import numpy as np
 from sklearn import preprocessing
+import seaborn as sns
 import pandas as pd
-from keras.models import Sequential
-from keras.layers import Dense
+
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+def distplot(feature, frame, color='g'):
+    plt.figure()
+    plt.title("Distribution for {}".format(feature))
+    ax = sns.histplot(frame[feature], color=color)
 
 if __name__ == '__main__':
     input_file = "./HCV-Egy-Data/HCV-Egy-Data.csv"
     df = pd.read_csv(input_file, header=0)
 
-    dataset = df.values
 
-    X = dataset[:, 0:28]
-    X=np.where(X=='?', 0, X)
+    # df describe
+    print(df.describe())
+    print(df.info()) # mi dice se ci sono tipi come 'object' e se qualche sample è nullo
+    X=df.drop(columns='Baselinehistological staging')
+    print("X:\n"+str(X))
 
-    # print(X)
-    Y = dataset[:, 28]
+    Y = df['Baselinehistological staging']
     Y = Y.astype(int)
     le = LabelEncoder()
     Y = le.fit_transform(Y)
+    print("Y:\n" + str(Y))
+    count_class_0 = 0
+    count_class_1 = 0
+    count_class_2 = 0
+    count_class_3 = 0
+    for i in Y:
+        # print(i)
+        if (i == 0):
+            count_class_0 += 1
+        if (i == 1):
+            count_class_1 += 1
+        if (i == 2):
+            count_class_2 += 1
+        if (i == 3):
+            count_class_3 += 1
 
-    '''
-    switch = {1: np.array([1, 0, 0, 0]),
-              2: np.array([0, 1, 0, 0]),
-              3: np.array([0, 0, 1, 0]),
-              4: np.array([0, 0, 0, 1])
-              }
-    Y_final = np.empty(shape=(1385, 4), dtype=int)
-    for i in range(len(Y)):
-        Y_final[i] = switch[Y[i]]
-    print(Y_final)
-    '''
-    param_grid = [
-        {
-            'activation': ['identity', 'logistic', 'tanh', 'relu'],
-            'solver': ['lbfgs', 'sgd', 'adam'],
-            'hidden_layer_sizes': [
-                (1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (10,), (11,), (12,), (13,), (14,), (15,), (16,),
-                (17,), (18,), (19,), (20,), (21,)
-            ]
-        }
-    ]
-    min_max_scaler = preprocessing.StandardScaler()
-    X_scale = min_max_scaler.fit_transform(X)
+    names_cols=X.columns
 
-    print("X:\n", X)
-    print("Y: \n", Y)
+
+
+    print(names_cols)
+    try:
+        for i in names_cols:
+            sns.displot(X, x=i)
+            plt.show()
+    except:
+        print("error to plot")
+    print("samples of class 0: " + str(count_class_0))
+    print("samples of class 1: " + str(count_class_1))
+    print("samples of class 2: " + str(count_class_2))
+    print("samples of class 3: " + str(count_class_3))
+    log_regr = LogisticRegression(penalty='l2', class_weight='balanced')
     X_train, X_val_and_test, Y_train, \
     Y_val_and_test = \
-        train_test_split(X_scale, Y, test_size=0.3)
+        train_test_split(X, Y, test_size=0.3)
     X_val, X_test, Y_val, Y_test = train_test_split \
         (X_val_and_test, Y_val_and_test, test_size=0.5)
-    print("Shape of whole dataset : " + str(dataset.shape))
-    min_max_scaler.fit(X_train)
-    X_train = min_max_scaler.transform(X_train)
-    X_test = min_max_scaler.transform(X_test)
-    # print(X_train.shape, X_val.shape, X_test.shape, Y_train.shape, Y_val.shape, Y_test.shape)
-    print("Shape of X train: " + str(X_train.shape) + "\nShape of Y train: " + str(Y_train.shape))
-    print("Shape of X valuation: " + str(X_val.shape) + "\nShape of Y valuation: " + str(Y_val.shape))
-    print("Shape of X test: " + str(X_test.shape) + "\nShape of Y test: " + str(Y_test.shape))
 
-    clf = MLPClassifier()
-    clf.fit(X_train, Y_train)
-
-    y_pred = clf.predict(X_test)
-    target_names = ['1', '2', '3', '4']
-    print(classification_report(Y_test, y_pred, target_names=target_names))
-    # print('Accuracy on the test set is: ' + str(np.round(accuracy_score(Y_test, y_pred) * 100, 2)) + '%')
-    print(y_pred)
-    # print(y_pred - Y_test)
-    '''
-    model = Sequential([
-        Dense(2, activation='relu', input_shape=(28,)),
-        Dense(2, activation='relu'),
-        Dense(1, activation='sigmoid'),
-    ])
-    model.compile(optimizer='sgd',
-                  loss='binary_crossentropy',
-                  metrics=['accuracy'])
-    hist = model.fit(X_train, Y_train,
-                     batch_size=2, epochs=10,
-                     validation_data=(X_val, Y_val))
-    model.evaluate(X_test, Y_test)[1]
-    plt.plot(hist.history['loss'])
-    plt.plot(hist.history['val_loss'])
-    plt.title('Model loss')
-    plt.ylabel('Loss')
-    plt.xlabel('Epoch')
-    plt.legend(['Train', 'Val'], loc='upper right')
-
-    plt.show()
-    
-    applicazione senza preprocessing dei dataset
-    applicazione con preprocessing
-    vedere quella migliore
-    unsambal
-    plottare le prestazioni
-    sezione dei risultati==> tabelle e plot==> 15/20 pagine
-    early stopping, weight decay
-    '''
+    #log_regr.fit(X_train, Y_train)
+    #y_pred = log_regr.predict(X_test)
+    #print(classification_report(Y_test, y_pred))
