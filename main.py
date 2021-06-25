43c6707214e5b7a8b5660b2436980b77a6753f0c
filippocasari <fili9941@gmail.com>
2021-06-25 11:30:27 +0200

    bella

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPClassifier
-import numpy as np
+
 from sklearn import preprocessing
 import seaborn as sns
 import pandas as pd
@@ -12,10 +8,11 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder, StandardScaler, LabelBinarizer, MinMaxScaler
 from Logistic_Regression import Logistic_regression
 
+
 def plot_metrics_for_each_features(names_cols, X, name_png):
     figures = []
     for i in names_cols:
-        figure = sns.displot(X, x=i, kind="kde")
+        figure = sns.displot(X, x=i)
         figures.append(figure)
         figure.savefig("./plots/" + str(i) + name_png)
         plt.close()  # plot close per chiudere la finestra di plot, onde evitare troppi  (>20)\
@@ -56,27 +53,30 @@ if __name__ == '__main__':
     count_class_3 = 0
     for i in Y:
         # print(i)
-        if (i == 0):
+        if i == 0:
             count_class_0 += 1
-        if (i == 1):
+        if i == 1:
             count_class_1 += 1
-        if (i == 2):
+        if i == 2:
             count_class_2 += 1
-        if (i == 3):
+        if i == 3:
             count_class_3 += 1
 
-    # nomi delle colonne
-    names_cols = X.columns
-    scaler = StandardScaler().fit(X[names_cols])
-    X_std = pd.DataFrame(scaler.transform(X[names_cols]), columns=names_cols)
+    # ----------------------------------PREPROCESSING----------------------------------------
+    names_cols = X.columns  # nomi delle colonne
+    scaler = StandardScaler()
+    X_std = pd.DataFrame(scaler.fit_transform(X[names_cols]), columns=names_cols)
 
-    min_max_scaler = preprocessing.StandardScaler()
+    min_max_scaler = MinMaxScaler()
     X_scale = pd.DataFrame(min_max_scaler.fit_transform(X[names_cols]), columns=names_cols)
     print(names_cols + "\n" + str(len(names_cols)))
     # plot and save images, not preprocessing
     plot_metrics_for_each_features(names_cols, X, "_not_preprocessing")
 
-    # plot and save images, with standardization
+    # plot and save images, with standardizationount_class_0 = 0
+    count_class_1 = 0
+    count_class_2 = 0
+    count_class_3 = 0
     plot_metrics_for_each_features(names_cols, X_std, "_standardized")
 
     # plot and save images, with min max scaler
@@ -86,6 +86,7 @@ if __name__ == '__main__':
     print("samples of class 1: " + str(count_class_1))
     print("samples of class 2: " + str(count_class_2))
     print("samples of class 3: " + str(count_class_3))
+    # ------------------------------------END PREPROCESSING-------------------------------------
 
     X_train, X_test, Y_train, Y_test = train_test_split(
         X, Y, random_state=0, train_size=0.66
@@ -94,7 +95,7 @@ if __name__ == '__main__':
         X_std, Y, random_state=0, train_size=0.66
     )
     X_train_minmax, X_test_minmax, Y_train, Y_test = train_test_split(
-        X, Y, random_state=0, train_size=0.66
+        X_scale, Y, random_state=0, train_size=0.66
     )
     print("Logistic regression without preprocessing:\n")
     Logistic_regression(X_train, Y_train, X_test, Y_test)
@@ -102,4 +103,3 @@ if __name__ == '__main__':
     Logistic_regression(X_train_std, Y_train, X_test_std, Y_test)
     print("Logistic regression with Min Max normalization:\n")
     Logistic_regression(X_train_minmax, Y_train, X_test_minmax, Y_test)
-
